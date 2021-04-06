40ed1581357a454e395dc84118651ab18d05d741
Bihan <direction5426@gmail.com>
2021-04-07 07:01:51 +0900

    MVC part one

---
+++ app.js
@@ -0,0 +1,24 @@
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import express from 'express';
+import userRouter from './routers/userRouter.js';
+import videoRouter from './routers/videoRouter.js';
+import globalRouter from './routers/globlaRouter.js';
+import routes from './routes.js';
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(morgan("tiny"));
+
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
